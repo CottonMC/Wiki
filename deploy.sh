1fbb8b116689b606a303446de58f174cf7f456b3
Meredith Espinosa <Boundarybreaker@users.noreply.github.com>
2019-10-25 22:31:42 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:falseresync/Wiki.git master:gh-pages
+git push -f git@github.com:cottonmc/Wiki.git master:gh-pages
 
 cd -
